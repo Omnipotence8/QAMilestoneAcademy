4cbf45b06b87092412172f3315348d5916a433a8
QAMilestoneAcademy <QAMilestoneAcademy@gmail.com>
2019-10-15 19:17:22 +0530

    add TC04_customCommand.spec.js, update cypress/support/commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("SignIn", () => {
+    cy.visit('https://react-redux.realworld.io/#/login')
+    cy.title().should('eq', 'Conduit')
+    cy.location('protocol').should('eq', 'https:')
+    cy.get('form').within(($form) => {
+        // cy.get() will only search for elements within form, not within the entire document
+        cy.get('input[type = "email"]').type('qamilestone.academy@gmail.com')
+        cy.get('input[type = "password"]').type('admin123')
+        cy.root().submit()   // submits the form yielded from 'within'
+    })
+    cy.contains('Your Feed', { timeout: 10000 }).should('be.visible')
+})
